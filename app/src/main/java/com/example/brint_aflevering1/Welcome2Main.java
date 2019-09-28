3a33ac2391e0550ebaaacd8a27c3f018ad5f1b76
HaraldBruun <s185014@student.dtu.dk>
2019-09-29 00:31:54 +0200

    Trying to implement welcomescreen with imageView and Welcome2Main button.

--- app/src/main/java/com/example/brint_aflevering1/Welcome2Main.java
+++ app/src/main/java/com/example/brint_aflevering1/Welcome2Main.java
@@ -1,4 +1,29 @@
 package com.example.brint_aflevering1;
 
-public class Welcome2Main {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Welcome2Main extends AppCompatActivity {
+    private Button knap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        knap = findViewById(R.id.button);
+        knap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+    private void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
